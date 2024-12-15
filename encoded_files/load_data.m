a79102be83d2d922b420cb92ce7709da5cc5554c
minh18ndh <minh.nguyendohoang@gmail.com>
2024-12-15 14:30:25 +0700

    Initial commit

---
+++ encoded_files/load_data.m
@@ -0,0 +1,12 @@
+data = load('encoded_file_k64.mat');
+
+% 'data' to view 'structure with one field named encoded_file contains a 512 × 254 double-precision matrix'
+
+encoded_matrix = data.encoded_file;
+
+% View first 2 rows
+%first_two_rows = data.encoded_file(1:2, :);
+%disp(first_two_rows);
+
+
+
